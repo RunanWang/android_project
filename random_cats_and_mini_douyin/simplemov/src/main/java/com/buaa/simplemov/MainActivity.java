c35b1a9db56d96e7fde16dd79125142e3a4fe43e
RyanWang <33189397+RyanMrW@users.noreply.github.com>
2019-07-06 22:44:41 +0800

    update mainActivity clear

--- random_cats_and_mini_douyin/simplemov/src/main/java/com/buaa/simplemov/MainActivity.java
+++ random_cats_and_mini_douyin/simplemov/src/main/java/com/buaa/simplemov/MainActivity.java
@@ -39,15 +39,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int PICK_IMAGE = 1;
-    private static final int PICK_VIDEO = 2;
     private static final String TAG = "DebugR";
     private RecyclerView mRv;
     private List<Feed> mFeeds = new ArrayList<>();
-    public Uri mSelectedImage;
-    private Uri mSelectedVideo;
-    public Button mBtn;
     private Button mBtnRefresh;
 
     private String[] mPermissionsArrays = new String[]{Manifest.permission.CAMERA,
@@ -62,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initRecyclerView();
-        initBtns();
+        mBtnRefresh = findViewById(R.id.btn_refresh);
         if (!checkPermissionAllGranted(mPermissionsArrays)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(mPermissionsArrays, REQUEST_PERMISSION);
@@ -70,31 +64,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(MainActivity.this, "已经获取所有所需权限", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    private void initBtns() {
-        mBtn = findViewById(R.id.btn);
-        mBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String s = mBtn.getText().toString();
-                if (getString(R.string.select_an_image).equals(s)) {
-                    chooseImage();
-                } else if (getString(R.string.select_a_video).equals(s)) {
-                    chooseVideo();
-                } else if (getString(R.string.post_it).equals(s)) {
-                    if (mSelectedVideo != null && mSelectedImage != null) {
-                        postVideo();
-                    } else {
-                        throw new IllegalArgumentException("error data uri, mSelectedVideo = " + mSelectedVideo + ", mSelectedImage = " + mSelectedImage);
-                    }
-                } else if ((getString(R.string.success_try_refresh).equals(s))) {
-                    mBtn.setText(R.string.select_an_image);
-                }
-            }
-        });
-
-        mBtnRefresh = findViewById(R.id.btn_refresh);
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -120,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
             public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
                 ImageView iv = (ImageView) viewHolder.itemView;
 
-                // TODO-C2 (10) Uncomment these 2 lines, assign image url of Feed to this url variable
                 String url = mFeeds.get(i).getImage_url();
                 //Log.d(TAG, "onBindViewHolder() called with: viewHolder = [" + viewHolder + "], i = [" + i + "]");
                 Glide.with(iv.getContext()).load(url).into(iv);
@@ -129,106 +97,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public int getItemCount() {
                 return mFeeds.size();
-            }
-        });
-    }
-
-    public void chooseImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
-                PICK_IMAGE);
-        mSelectedImage = intent.getData();
-    }
-
-
-    public void chooseVideo() {
-        Intent intent = new Intent();
-        intent.setType("video/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Video"),
-                PICK_VIDEO);
-        mSelectedVideo = intent.getData();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Log.d(TAG, "onActivityResult() called with: requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "]");
-
-        if (resultCode == RESULT_OK && null != data) {
-
-            if (requestCode == PICK_IMAGE) {
-                mSelectedImage = data.getData();
-                Log.d(TAG, "selectedImage = " + mSelectedImage);
-
-                mBtn.setText(R.string.select_a_video);
-            } else if (requestCode == PICK_VIDEO) {
-                mSelectedVideo = data.getData();
-                Log.d(TAG, "mSelectedVideo = " + mSelectedVideo);
-
-                mBtn.setText(R.string.post_it);
-            }
-        }
-    }
-
-    private MultipartBody.Part getMultipartFromUri(String name, Uri uri) {
-        // if NullPointerException thrown, try to allow storage permission in system settings
-        File f = new File(ResourceUtils.getRealPath(MainActivity.this, uri));
-        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), f);
-        return MultipartBody.Part.createFormData(name, f.getName(), requestFile);
-    }
-
-    private void postVideo() {
-        mBtn.setText("POSTING...");
-        mBtn.setEnabled(false);
-
-        ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE},
-                1);
-
-        MultipartBody.Part coverImage = getMultipartFromUri("cover_image", mSelectedImage);
-        MultipartBody.Part video = getMultipartFromUri("video", mSelectedVideo);
-        // if success, make a text Toast and show
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://test.androidcamp.bytedance.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-
-        Call<PostVideoResponse> call = retrofit.create(IMiniDouyinService.class).createVideo(
-                "16161616", "Mouse",
-                coverImage,
-                video
-        );
-
-        call.enqueue(new Callback<PostVideoResponse>() {
-
-            @Override
-            public void onResponse(Call<PostVideoResponse> call, Response<PostVideoResponse> response) {
-                ;
-                //Log.d(TAG, "getCat() get cat = [" + call + "]");
-                //ArrayList<Cat> cats = new ArrayList<>();
-                //cats.add(catTransfer);
-                //Log.d(TAG, "onResponse() called with: call = [" + call + "], response = [" + response + "]");
-                PostVideoResponse response1 = response.body();
-                if (response1.isSuccess()) {
-                    Log.d(TAG, "post is OK ");
-                }
-                Log.d(TAG, "response is" + response1);
-                Toast.makeText(MainActivity.this,"post success!",Toast.LENGTH_SHORT).show();
-                mBtn.setText(R.string.select_an_image);
-                mBtn.setEnabled(true);
-            }
-
-            @Override
-            public void onFailure(Call<PostVideoResponse> arg0, Throwable arg1) {
-                Log.d(TAG, "onFailure() called with: arg0 = [" + arg0 + "], arg1 = [" + arg1 + "]");
-                mBtn.setText(R.string.select_an_image);
-                mBtn.setEnabled(true);
             }
         });
     }
