345406814ff1fb99de3501d997b86861b1648b7f
陈进宇 <438006760@qq.com>
2019-07-08 23:46:20 +0800

    costum camera

--- random_cats_and_mini_douyin/simplemov/src/main/java/com/buaa/simplemov/UploadActivity.java
+++ random_cats_and_mini_douyin/simplemov/src/main/java/com/buaa/simplemov/UploadActivity.java
@@ -6,8 +6,11 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
 import android.media.ExifInterface;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -16,11 +19,19 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import com.buaa.simplemov.bean.PostVideoResponse;
 import com.buaa.simplemov.newtork.IMiniDouyinService;
 import com.buaa.simplemov.utils.ResourceUtils;
@@ -40,11 +51,15 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.buaa.simplemov.utils.Utils.MEDIA_TYPE_VIDEO;
+import static com.buaa.simplemov.utils.Utils.getOutputMediaFile;
+
 public class UploadActivity extends AppCompatActivity {
     private ActionSheetDialog imageDialog;
     private ActionSheetDialog videoDialog;
     private LoadingDialog loadingDialog;
     private ImageView done;
+    private MediaRecorder mMediaRecorder;
     private final int INIT_IMAGE = 1;
     private final int INIT_VIDEO = 2;
     private final int INIT_POST = 3;
@@ -52,10 +67,18 @@ public class UploadActivity extends AppCompatActivity {
     private final int PICK_VIDEO = 2;
     private final int TAKE_PHOTO = 3;
     private final int TAKE_VIDEO = 4;
+    private static final int DEGREE_90 = 90;
+    private static final int DEGREE_180 = 180;
+    private static final int DEGREE_270 = 270;
+    private static final int DEGREE_360 = 360;
     private Uri imageUri;
     private Uri videoUri;
     private File imageFile;
     private File videoFile;
+    private SurfaceView mSurfaceView;
+    private Camera mCamera;
+    private int rotationDegree = 0;
+    private int CAMERA_TYPE = Camera.CameraInfo.CAMERA_FACING_BACK;
     private String[] mPermissionsArrays = new String[]{Manifest.permission.CAMERA,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -125,6 +148,9 @@ public class UploadActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_upload);
         if (!checkPermissionAllGranted(mPermissionsArrays)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -186,8 +212,136 @@ public class UploadActivity extends AppCompatActivity {
                 videoView.setVisibility(View.GONE);
             }
         });
+        initCamera();
+        final ImageView record = findViewById(R.id.record);
+        record.setOnClickListener(new View.OnClickListener() {
+            boolean recording = false;
+            @Override
+            public void onClick(View v) {
+                if(!recording){
+                   if(!prepareVideoRecorder()){
+                       Toast.makeText(UploadActivity.this,
+                               "error to start record",Toast.LENGTH_SHORT).show();
+                   }
+                   recording  = true;
+                   done = findViewById(R.id.video);
+                   done.setImageResource(R.drawable.ic_add_to_queue_black_24dp);
+                }else{
+                    releaseMediaRecorder();
+                    recording = false;
+                    done = findViewById(R.id.video);
+                    done.setImageResource(R.drawable.ic_check_black_24dp);
+                }
+            }
+        });
+    }
+    void initCamera(){
+        mSurfaceView = findViewById(R.id.img);
+        //todo 给SurfaceHolder添加Callback
+        SurfaceHolder surfaceHolder = mSurfaceView.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    mCamera = getCamera(CAMERA_TYPE);
+                    mCamera.setPreviewDisplay(surfaceHolder);
+                    mCamera.startPreview();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                releaseCameraAndPreview();
+            }
+        });
+    }
+    public Camera getCamera(int position) {
+        CAMERA_TYPE = position;
+        if (mCamera != null) {
+            releaseCameraAndPreview();
+        }
+        Camera cam = Camera.open(position);
+        //todo 摄像头添加属性，例是否自动对焦，设置旋转方向等
+        int degree =  getCameraDisplayOrientation(position);
+        rotationDegree = degree;
+        cam.setDisplayOrientation(rotationDegree);
+        return cam;
+    }
+    private int getCameraDisplayOrientation(int cameraId) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = DEGREE_90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = DEGREE_180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = DEGREE_270;
+                break;
+            default:
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % DEGREE_360;
+            result = (DEGREE_360 - result) % DEGREE_360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + DEGREE_360) % DEGREE_360;
+        }
+        return result;
+    }
+    private void releaseCameraAndPreview() {
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
     }
 
+    private boolean prepareVideoRecorder() {
+        //todo 准备MediaRecorder
+        mMediaRecorder = new MediaRecorder();
+        mCamera.unlock();
+        mMediaRecorder.setCamera(mCamera);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        videoFile =  getOutputMediaFile(MEDIA_TYPE_VIDEO);
+        mMediaRecorder.setOutputFile(videoFile.toString());
+        mMediaRecorder.setPreviewDisplay(mSurfaceView.getHolder().getSurface());
+        mMediaRecorder.setOrientationHint(rotationDegree);
+        try {
+            mMediaRecorder.prepare();
+            mMediaRecorder.start();
+            return true;
+
+        }catch (Exception e){
+            releaseMediaRecorder();
+            return false;
+        }
+    }
+    private void releaseMediaRecorder() {
+        //todo 释放MediaRecorder
+        mMediaRecorder.stop();
+        mMediaRecorder.reset();
+        mMediaRecorder.release();
+        mMediaRecorder = null;
+        mCamera.lock();
+    }
     void findImageInAlbum() {
         Toast.makeText(this, "in find photo", Toast.LENGTH_SHORT).show();
         Intent findImage = new Intent();
@@ -200,7 +354,7 @@ public class UploadActivity extends AppCompatActivity {
 
     private void takePhoto() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        imageFile = Utils.getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
+        imageFile = getOutputMediaFile(Utils.MEDIA_TYPE_IMAGE);
         if (imageFile != null) {
             imageUri = FileProvider.getUriForFile(this, "com.bytedance.simplemov", imageFile);
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
@@ -305,8 +459,6 @@ public class UploadActivity extends AppCompatActivity {
         return  Bitmap.createBitmap(bitmap,0,0,
                 bitmap.getWidth(),bitmap.getHeight(),matrix,true);
     }
-
-
     //    private MultipartBody.Part getMultipartFromUri(String name, Uri uri) {
 //        // if NullPointerException thrown, try to allow storage permission in system settings
 //        try {
@@ -366,4 +518,6 @@ public class UploadActivity extends AppCompatActivity {
                     }
                 });
     }
+
+
 }
